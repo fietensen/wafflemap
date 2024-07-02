03c134bae87d81a0e6c570f06ebebb984c7e3302
fietensen <fietensen@gmail.com>
2024-07-02 17:13:34 +0200

    initial spring structure

---
+++ backend/src/main/java/de/fietensen/wafflemap/WafflemapApplication.java
@@ -0,0 +1,13 @@
+package de.fietensen.wafflemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WafflemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WafflemapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/fietensen/wafflemap/WafflemapApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fietensen.wafflemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WafflemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
