2efbc0c96da8cc41922f875bba04d3025e4a25f2
fietensen <fietensen@gmail.com>
2024-07-03 11:53:53 +0200

    add map container to map component

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -1,6 +1,14 @@
+import { TileLayer } from 'react-leaflet/TileLayer';
+import { MapContainer } from 'react-leaflet/MapContainer';
+
 function Map() {
     return (
-        <div>Map</div>
+        <MapContainer>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
     )
 }
 
