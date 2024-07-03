7d011afb66945cce4325cfd0b93bf8f9924da4f5
fietensen <fietensen@gmail.com>
2024-07-03 19:42:51 +0200

    add children prop

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -1,7 +1,8 @@
 import { TileLayer } from 'react-leaflet/TileLayer';
 import { MapContainer } from 'react-leaflet/MapContainer';
+import PropTypes from 'prop-types';
 
-function Map() {
+function Map({ children }) {
     return (
         <MapContainer style={{
             width: '100vw',
@@ -11,8 +12,13 @@ function Map() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            {children}
         </MapContainer>
     )
 }
+
+Map.propTypes = {
+    children: PropTypes.node,
+};
 
 export default Map
