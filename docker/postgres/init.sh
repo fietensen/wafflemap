7d8e32d0d69acfff89aee3b142eaa365799f4b35
fietensen <fietensen@gmail.com>
2024-07-04 17:42:47 +0200

    add -u option, closes #34

--- docker/postgres/init.sh
+++ docker/postgres/init.sh
@@ -9,4 +9,4 @@ until psql -v ON_ERROR_STOP=1 -U "${WM_POSTGRES_USER}" -d "${WM_POSTGRES_DB}" -c
 done
 unset PGPASSWORD
 
-python3 /wafflemap_db_init/download_insert_osm_data.py
+python3 -u /wafflemap_db_init/download_insert_osm_data.py
