ef398c23f6ac63e7c206c16fa32ec6e0e9cebfb7
fietensen <fietensen@gmail.com>
2024-07-08 18:08:16 +0200

    add findRoadsByName method

--- backend/src/main/java/de/fietensen/wafflemap/repository/RoadSearchRepository.java
+++ backend/src/main/java/de/fietensen/wafflemap/repository/RoadSearchRepository.java
@@ -1,6 +1,6 @@
 package de.fietensen.wafflemap.repository;
 
-import de.fietensen.wafflemap.model.RoadEdge;
+import de.fietensen.wafflemap.model.DummyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,10 +9,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface RoadLocationRepository extends JpaRepository<RoadEdge, Long> {
-    @Query(value = "SELECT name, x, y FROM (SELECT name, u, v, ROW_NUMBER() OVER(PARTITION BY name ORDER BY ID DESC) rn FROM public.location_roads_edges) b INNER JOIN public.location_roads_verts a ON u = a.osmid WHERE rn = 1 AND name ILIKE %?1% AND NOT SUBSTRING(name, 1, 1) LIKE '[';", nativeQuery = true)
-    List<Object[]> findMatchingItems(String searchTerm);
-
-    @Query(value = "SELECT edge.id, edge.x, edge.y, edge.geometry <-> ST_SetSRID(ST_MakePoint(:lon, :lat), 4326) as dist FROM public.location_roads_verts edge ORDER BY dist LIMIT 1;", nativeQuery = true)
-    Object[] findNearestVertex(@Param("lon") Double x, @Param("lat") Double y);
+public interface RoadSearchRepository extends JpaRepository<DummyEntity, Long> {
+    @Query(value = "SELECT name, x, y FROM (SELECT name, u, v, ROW_NUMBER() OVER(PARTITION BY name ORDER BY ID DESC) AS rn FROM \"public\".location_roads_edges) b INNER JOIN \"public\".location_roads_verts a ON u = a.osmid WHERE rn = 1 AND name ILIKE %:name% AND NOT SUBSTRING(name, 1, 1) LIKE '['  LIMIT :limit OFFSET :skip", nativeQuery = true)
+    List<Object[]> findRoadsByName(@Param("name") String name,
+                                   @Param("limit") Long limit,
+                                   @Param("skip") Long skip);
 }
