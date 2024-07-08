5b7c2cfa92bd49095251a6c508075879dad7caad
fietensen <fietensen@gmail.com>
2024-07-08 15:01:28 +0200

    implement RoadDistDTO

--- backend/src/main/java/de/fietensen/wafflemap/dto/RoadDistDTO.java
+++ backend/src/main/java/de/fietensen/wafflemap/dto/RoadDistDTO.java
@@ -1,4 +1,11 @@
 package de.fietensen.wafflemap.dto;
 
+import lombok.Data;
+
+@Data
 public class RoadDistDTO {
+    private Double distance;
+    private Long id;
+    private Double lat;
+    private Double lon;
 }
