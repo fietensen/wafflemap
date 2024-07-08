ffb897358877bd5876f48cb270a27458cc1652f1
fietensen <fietensen@gmail.com>
2024-07-08 15:15:36 +0200

    add RoadNode entity

---
+++ backend/src/main/java/de/fietensen/wafflemap/model/RoadNode.java
@@ -0,0 +1,29 @@
+package de.fietensen.wafflemap.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import org.locationtech.jts.geom.Geometry;
+
+@Getter
+@Entity
+@Table(schema = "public", name = "location_roads_verts")
+public class RoadNode {
+    @Id
+    private Long id;
+
+    @Column(name = "osmid")
+    private Long osmId;
+
+    private Double y;
+    private Double x;
+
+    @Column(name = "street_count")
+    private Long streetCount;
+
+    private String ref;
+    private String highway;
+    private Geometry geometry;
+}
