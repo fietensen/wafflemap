1fa6291be1bbf7168fd307ee94aac278ab48d806
fietensen <fietensen@gmail.com>
2024-07-08 14:59:54 +0200

    implement RoadResultsDTO

--- backend/src/main/java/de/fietensen/wafflemap/dto/RoadResultsDTO.java
+++ backend/src/main/java/de/fietensen/wafflemap/dto/RoadResultsDTO.java
@@ -1,4 +1,12 @@
 package de.fietensen.wafflemap.dto;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class RoadResultsDTO {
+    // Link to next Results (generated based on limit)
+    private String next;
+    private List<RoadResultDTO> results;
 }
