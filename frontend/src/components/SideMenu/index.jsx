9e2641be1e712f2c2bbaa8187c8ae3b47b1885af
fietensen <fietensen@gmail.com>
2024-07-27 20:42:00 +0200

    implement waffle config, closes #59

--- frontend/src/components/SideMenu/index.jsx
+++ frontend/src/components/SideMenu/index.jsx
@@ -1,12 +1,16 @@
 import { useCallback, useEffect, useState } from 'react';
 import RoadLocationInput from '../RoadLocationInput';
-import { Wrapper, BoxWrapper } from './styles.module.css';
+import { Wrapper, BoxWrapper, Title, TitleWrapper } from './styles.module.css';
 import PropTypes from 'prop-types';
+import TravelConfigurator from '../TravelConfigurator';
+import WaffleResult from '../WaffleResult';
 
 function SideMenu({ showRoute }) {
     const [currentRoadInputElement, setCurrentRoadInputElement] = useState('');
     const [startRoadSelected, setStartRoadSelected] = useState(null);
     const [destRoadSelected, setDestRoadSelected] = useState(null);
+    const [travelConfig, setTravelConfig] = useState(null);
+    const [routeResult, setRouteResult] = useState(null);
 
     useEffect(() => {
         if (!(startRoadSelected && destRoadSelected))
@@ -16,6 +20,7 @@ function SideMenu({ showRoute }) {
             .then((result) => (result.json()))
             .then((result) => {
                 showRoute(result);
+                setRouteResult(result);
             })
             .catch((err) => {
                 console.error(err);
@@ -25,6 +30,12 @@ function SideMenu({ showRoute }) {
 
     return (
         <Wrapper>
+            <BoxWrapper>
+                <TitleWrapper>
+                    <Title>Configuration</Title>
+                </TitleWrapper>
+                <TravelConfigurator setTravelConfig={setTravelConfig} />
+            </BoxWrapper>
             <BoxWrapper>
                 <RoadLocationInput
                     setCurrentRoadInputElement={setCurrentRoadInputElement}
@@ -39,6 +50,13 @@ function SideMenu({ showRoute }) {
             </BoxWrapper>
             {
                 currentRoadInputElement
+            }
+            {
+                (routeResult && travelConfig) ?
+                    <BoxWrapper>
+                        <WaffleResult distance={routeResult.distance} wafflePrice={travelConfig.wafflePrice} travelCost={travelConfig.travelCost} />
+                    </BoxWrapper>
+                    : ''
             }
 
         </Wrapper>
